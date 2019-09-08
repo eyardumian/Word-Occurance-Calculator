a5bb1cf384f1cf194255667a5e24b38780843be6
Erika Yardumian <eyardumian@hotmail.com>
2019-09-07 22:22:38 -0700

    Add variables to WordOccurance class

---
+++ WordCalculator/WordOccurences.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCalculator
+{
+    class WordOccurences
+    {
+        public String word;
+        public int count;
+    }
+}
