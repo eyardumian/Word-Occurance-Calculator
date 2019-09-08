f8ee6272ceaa3a1b3a3bf5030e919b004b4db6b3
Erika Yardumian <eyardumian@hotmail.com>
2019-09-07 22:42:05 -0700

    Created methods and form controls

--- WordCalculator/Form1.cs
+++ WordCalculator/Form1.cs
@@ -16,5 +16,10 @@ namespace WordCalculator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
     }
 }
---
+++ WordCalculator/WordCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCalculator
+{
+    class WordCalculator
+    {
+    }
+
+    public static CalculateOccurances (List<string> WordOccurrence)
+    {
+
+    }
+}
